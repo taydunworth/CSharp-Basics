38295ebadfa67fae30a4b1402aa6347b9cc2ef32
Taylor Cox <taylorcox@Taylors-MacBook-Pro.local>
2019-06-26 16:52:12 -0400

    Adds solution for exercise one

---
+++ Arrays-and-Lists/Exercise-1/Exercise-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information.
+
+            // • If no one likes your post, it doesn't display anything.
+            // • If only one person likes your post, it displays: [Friend's Name] likes your post.
+            // • If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
+            // • If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number of Other People] others like your post.
+
+            // Write a program and continuously ask the user to enter different names, until the user presses Enter (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
+
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Type a name or press enter to quit: ");
+
+                var userInput = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(userInput))
+                    break;
+                names.Add(userInput);
+            }
+
+            if (names.Count > 2)
+                Console.WriteLine("{0}, {1}, and {2} others like your post.", names[0], names[1], names.Count - 2);
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} like your post.", names[0], names[1]);
+            else if (names.Count == 1)
+                Console.WriteLine("{0} likes your post.", names[0]);
+            else
+                Console.WriteLine();
+        }
+    }
+}
