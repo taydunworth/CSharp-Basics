8d7ea7d7fb08ebc40d1fe8482ecca63c4304aa71
Taylor Cox <taylorcox@Taylors-MacBook-Pro.local>
2019-06-26 16:21:21 -0400

    Adds Arrays and Lists folder and renames Conditionals folder

---
+++ Conditionals/Exercise-2/Exercise-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program which takes two numbers from the console and displays the maximum of the two.
+
+            Console.Write("Please enter a number: ");
+            var numOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter another number: ");
+            var numTwo = Convert.ToInt32(Console.ReadLine());
+            var maxNum = (numOne > numTwo) ? numOne : numTwo;
+            Console.WriteLine("The larger number of the two is " + maxNum);
+        }
+    }
+}
