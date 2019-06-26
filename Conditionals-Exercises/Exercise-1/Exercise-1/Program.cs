7ff42bbe8ee53392f4227174e5de6c047180349c
Taylor Cox <taylorcox@Taylors-MacBook-Pro.local>
2019-06-26 09:41:14 -0400

    Creates repo and adds exercise one

---
+++ Conditionals-Exercises/Exercise-1/Exercise-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program which takes two numbers from the console and displays the maximum of the two.
+
+            Console.Write("Please enter a number: ");
+            var numOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter another number: ");
+            var numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numOne + numTwo);
+        }
+    }
+}
