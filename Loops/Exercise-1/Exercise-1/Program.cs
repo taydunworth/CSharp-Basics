71ecb65d351ee9e62174c9f97d8c8fa314c08dec
Taylor Cox <taylorcox@Taylors-MacBook-Pro.local>
2019-06-26 10:06:46 -0400

    Adds loop exercise folder with solution for exercise one

---
+++ Loops/Exercise-1/Exercise-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
+
+            var counter = 0;
+            for (var i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    counter++;
+            }
+            Console.WriteLine("There are {0} numbers between 1 and 100 that are divisible by 3", counter);
+        }
+    }
+}
