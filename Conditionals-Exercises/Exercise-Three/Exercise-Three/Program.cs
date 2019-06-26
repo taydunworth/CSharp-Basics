f9f248513f4fcf066fe36d44ca46238c2fdf8bf7
Taylor Cox <taylorcox@Taylors-MacBook-Pro.local>
2019-06-26 09:51:52 -0400

    Adds exercise three

---
+++ Conditionals-Exercises/Exercise-Three/Exercise-Three/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
+
+            Console.Write("Enter the width of your image: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the height of your image: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            if (width > height)
+                Console.WriteLine("Your image is landscape.");
+            else if (width < height)
+                Console.WriteLine("Your image is portrait.");
+            else
+                Console.WriteLine("Your image is a square.");
+
+        }
+    }
+}
