08cbb5a31c7eb2f0c917959ea535d590339415f4
Taylor Cox <taylorcox@Taylors-MacBook-Pro.local>
2019-06-27 10:18:02 -0400

    Adds solution to exercise three

---
+++ Arrays-and-Lists/Exercise-3/Exercise-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+
+            var numberList = new List<int>();
+
+            while (numberList.Count < 5)
+            {
+                Console.Write("Please enter a number: ");
+                var newNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (numberList.Contains(newNumber))
+                {
+                    Console.WriteLine("You've already entered that number.");
+                    continue;
+                }
+                else
+                    numberList.Add(newNumber);
+            }
+
+            numberList.Sort();
+
+            foreach ( var number in numberList)
+                Console.WriteLine(number);
+        }
+    }
+}
