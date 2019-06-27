b17dc5b771c96d5cd83c6a22e686a920788e8752
Taylor Cox <taylorcox@Taylors-MacBook-Pro.local>
2019-06-27 09:43:26 -0400

    Adds solution to exercise two

---
+++ Arrays-and-Lists/Exercise-2/Exercise-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program and ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. Display the reversed name on the console.
+
+            Console.Write("Please enter your name: ");
+            var name = Console.ReadLine();
+            var nameArray = new char[name.Length];
+
+            for (var i = name.Length; i > 0; i--)
+                nameArray[name.Length - i] = name[i - 1];
+
+            var reversed = new string(nameArray);
+            Console.WriteLine("Your name reversed is " + reversed);
+        }
+    }
+}
